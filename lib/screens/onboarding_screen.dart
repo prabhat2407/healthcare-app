314e48ccb3cf0ae00d86b9b34ab8d8aa5e0f143d
Prabhat <prabhatgprai@gmail.com>
2021-11-20 00:10:22 +0530

    Initial commit

---
+++ lib/screens/onboarding_screen.dart
@@ -0,0 +1,85 @@
+import 'package:doctor_consultation_app/constant.dart';
+import 'package:doctor_consultation_app/screens/signin.dart';
+import 'package:flutter/material.dart';
+
+class OnboardingScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kBackgroundColor,
+      body: SafeArea(
+        bottom: false,
+        child: Stack(
+          children: <Widget>[
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Image.asset(
+                'assets/images/onboarding_illustration.png',
+                width: MediaQuery.of(context).size.width,
+                fit: BoxFit.fitWidth,
+              ),
+            ),
+            Positioned(
+              top: MediaQuery.of(context).size.height / 6,
+              child: Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: MediaQuery.of(context).size.width / 8,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      'HealFirst',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 32,
+                        color: kTitleTextColor,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      'Health comes first.',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: kTitleTextColor.withOpacity(0.7),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    MaterialButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => SignIn(),
+                          ),
+                        );
+                      },
+                      color: kOrangeColor,
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 30,
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Text(
+                        "Let's Get Started",
+                        style: TextStyle(
+                          color: kWhiteColor,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
